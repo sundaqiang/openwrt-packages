97eb604a4d678e89e52fc25ce5b5dbb947fa3c53
sundaqiang <393116105@qq.com>
2021-11-27 00:44:11 +0800

    Add backup multiple files

--- luci-app-supervisord/luasrc/controller/supervisord.lua
+++ luci-app-supervisord/luasrc/controller/supervisord.lua
@@ -15,6 +15,31 @@ function index()
     entry({"admin", "services", "supervisord", "getlog"}, call("getlog")).leaf = true
 end
 
+function Split(str, delim, maxNb)  
+    -- Eliminate bad cases...  
+    if string.find(str, delim) == nil then 
+        return { str } 
+    end 
+    if maxNb == nil or maxNb < 1 then 
+        maxNb = 0    -- No limit  
+    end 
+    local result = {} 
+    local pat = "(.-)" .. delim .. "()"  
+    local nb = 0 
+    local lastPos  
+    for part, pos in string.gfind(str, pat) do 
+        nb = nb + 1 
+        result[nb] = part  
+        lastPos = pos  
+        if nb == maxNb then break end 
+    end 
+    -- Handle the last field  
+    if nb ~= maxNb then 
+        result[nb + 1] = string.sub(str, lastPos)  
+    end 
+    return result  
+end 
+
 function status()
 	local e = {}
 	e.running = luci.sys.call("ps | grep supervisord | grep -v grep >/dev/null") == 0
@@ -175,9 +200,12 @@ function savetask()
         if not sysupgrade:find(file) then
             sysupgrade=sysupgrade .. '\n' .. file
         end
-        backupfile=data:match("directory=([%a%d%p ]+)") .. "/" .. data:match("backupfile=([%a%d%p]+)")
-        if not sysupgrade:find(backupfile:gsub("%p", "%%%1")) then
-            sysupgrade=sysupgrade .. '\n' .. backupfile
+        backupfile=data:match("backupfile=([%a%d%p]+)")
+        backupfile=Split(backupfile, "||")
+        for k, v in ipairs(backupfile) do      
+            if not sysupgrade:find(v:gsub("%p", "%%%1")) then
+                sysupgrade=sysupgrade .. '\n' .. v
+            end
         end
         nixio.fs.writefile ("/etc/sysupgrade.conf", sysupgrade)
         luci.sys.call("supervisord ctl reload")
